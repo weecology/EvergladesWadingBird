42891264f7c1fef20f8170faf5a64fd884f692e4
Glenda Yenni <glenda@weecology.org>
2020-02-04 18:11:39 -0700

    Add weather and water data from external sources

---
+++ install-packages.R
@@ -0,0 +1,6 @@
+# Install pacman if it isn't already installed
+if ("pacman" %in% rownames(installed.packages()) == FALSE) install.packages("pacman")
+
+# Install packages required for analysis
+
+pacman::p_load(git2r, httr, semver, testthat, yaml)
