b08e2d7cf9647663d927cfc1e44586dfab69d1f8
bw4sz <benweinstein2010@gmail.com>
2020-03-17 20:33:38 -0700

    debugging on hipergator

--- Zooniverse/manifest.py
+++ Zooniverse/manifest.py
@@ -8,24 +8,28 @@ import tile_raster
 #Detect new files since last run
 def find_files(path):
     images = {}
-    image_paths = glob.glob(path + "*.png")
+    image_paths = glob.glob(os.path.join(path, "*.tif"))
     counter = 1
     for i in image_paths:
         images[i] = {"subject_reference":counter}
         counter +=1
-        
-#TODO hash this password.
+    
+    return images
+    
 def connect():
+    #TODO hash this password.    
     Panoptes.connect(username='bw4sz', password='D!2utNBno8;b')
+    everglades_watch = Project.find(10951)
+    return everglades_watch
 
 #Create manifests
-def create_subject_set(name="demo"):
+def create_subject_set(everglades_watch, name="demo"):
     subject_set = SubjectSet()
-    subject_set.links.project = "Everglades Watch"
+    subject_set.links.project = everglades_watch
     subject_set.display_name = name
     subject_set.save()    
 
-def upload(subject_set, images):
+def upload(subject_set, images, everglades_watch):
     """Assign images to projecti"""
     new_subjects = []
     
@@ -33,7 +37,7 @@ def upload(subject_set, images):
     for filename, metadata in images.items():
         subject = Subject()
     
-        subject.links.project = tutorial_project
+        subject.links.project = everglades_watch
         subject.add_location(filename)
     
         subject.metadata.update(metadata)
@@ -43,28 +47,39 @@ def upload(subject_set, images):
         new_subjects.append(subject)    
     subject_set.add(new_subjects)
 
-def main(path):
+def main(path, everglades_watch, save_dir="/orange/ewhite/everglades/Zooniverse/"):
     """Args:
         path: a .tif to run
     """
     
+    #Create new directory in save_dir
+    basename = os.path.splitext(os.path.basename(path))[0]
+    dirname = "{}/{}".format(save_dir,basename)
+    
+    try:
+        os.mkdir(dirname)
+    except:
+        raise ValueError("dirname: {} exists)".format(dirname))
+    
     #Crop tif
-    saved_file = tile_raster.run(path, save_dir)
+    saved_file = tile_raster.run(path, dirname)
+    print("Created cropped files at {}".format(saved_file))
     
     #Generate metadata
     images = find_files(saved_file)
+    print("{} images ready for upload".format(len(images)))
     
     #Create a new subject set
-    subject_set = create_subject_set()
+    subject_set = create_subject_set(name=basename, everglades_watch=everglades_watch)
     
     #Upload
-    upload(subject_set, images)
+    upload(subject_set, images, everglades_watch)
     
     return saved_file
 
 if __name__ == "__main__":
     #auth
-    connect()    
+    everglades_watch = connect()    
     
     #Which files have already been run
     uploaded = pd.read_csv("uploaded.csv")
@@ -74,12 +89,13 @@ if __name__ == "__main__":
     
     #Files to process
     file_pool = glob.glob("/orange/ewhite/everglades/WadingBirds2020/**/*.tif",recursive=True)
-    file_pool = [os.path.basename(x) for x in file_pool]
-    paths = [x for x in file_pool if not x in uploaded.basename.values]
-    
+    file_pool_basenames = [os.path.basename(x) for x in file_pool]
+    paths = [file_pool[index] for index, x in enumerate(file_pool_basenames) if not x in uploaded.basename.values]
+        
+    print("Running files:{}".format(paths))
     for path in paths:
         #Run .tif
-        saved_file=main(path)
+        saved_file = main(path, everglades_watch)
         
         #Confirm it exists and write to the csv file
         assert os.path.exists(saved_file)
