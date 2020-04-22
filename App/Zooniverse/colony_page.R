e3f35ca489efa37ff1e4bb99820359eaca111cb1
bw4sz <benweinstein2010@gmail.com>
2020-04-21 17:35:05 -0700

    adding colony data

--- App/Zooniverse/colony_page.R
+++ App/Zooniverse/colony_page.R
@@ -3,7 +3,7 @@ colony_page<-function(selected_boxes){
   
   renderUI({
     fluidPage(
-      titlePanel("Coming Soon: Visit the colonies!"),
+      titlePanel("Visit the colonies!"),
       selectizeInput("selected_image", "Site", image_list, selected = "6thBridge 2020-03-11", multiple = TRUE,options = NULL),
       leafletOutput("colony_map")
     )})
