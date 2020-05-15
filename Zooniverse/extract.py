813044f7379a7fd93f591b206abd9671567b0ea3
bw4sz <benweinstein2010@gmail.com>
2020-05-14 21:15:39 -0700

    update gitignore

---
+++ Zooniverse/extract.py
@@ -0,0 +1,69 @@
+#Extract old zooniverse annotations. We had a test workflow on bounding boxes, let's salvage what we an
+import pandas
+import paramiko
+import os
+import geopandas as gp
+import everglade_tokens
+import requests
+import json
+from shapely.geometry import box
+
+#source keys outside of git control
+import everglade_tokens
+
+def hipergator_auth():
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    
+    client.connect(everglade_tokens.get_ip(), username=everglade_tokens.get_username(),password=everglade_tokens.get_password())
+    return client
+
+def download_from_zooniverse(name, url):
+    
+    with open(name, 'wb') as handle:
+        response = requests.get(url, stream=True)
+    
+        if not response.ok:
+            print(response)
+    
+        for block in response.iter_content(1024):
+            if not block:
+                break
+    
+            handle.write(block)
+            
+def run(classification_shp, image_data ,savedir="."):
+    """
+    classification_shp: path to a processed .shp, see aggregate.py
+    image_data: subject id download from zooniverse everglades-watch-subjects.csv
+    """
+    #Read in species data
+    df = gp.read_file(classification_shp)
+    df = df[["subject_id","x","y","species","behavior","geometry"]]
+    df.subject_id = df.subject_id.astype(int)
+    
+    #Read in image location data
+    image_df = pandas.read_csv(image_data)
+    image_df = image_df[["subject_id","locations"]]
+    joined_df = df.merge(image_df,on="subject_id")
+        
+    joined_df["url"] = joined_df.locations.apply(lambda x: json.loads(x)['0'])
+    grouped_df = joined_df.groupby("url")
+    
+    #Split into image groups and download the image and write a shapefile
+    group_data = [grouped_df.get_group(x) for x in grouped_df.groups]
+    
+    for group in group_data:
+        
+        #Format for download
+        download_url = group.url.unique()[0]
+        
+        #Download image
+        basename = "{}".format(group.subject_id.unique()[0])
+        name = "{}.png".format(os.path.join(savedir,basename))
+        download_from_zooniverse(name=name, url=download_url)
+        group["geometry"] = [box(left, bottom, right, top) for left, bottom, right, top in group.geometry.buffer(1).bounds.values]
+        
+        #Create a shapefile
+        shpname = "{}.shp".format(os.path.join(savedir,basename))
+        group.to_file(shpname)    
--- Zooniverse/tests/test_extract.py
+++ Zooniverse/tests/test_extract.py
@@ -3,7 +3,7 @@ import os
 import sys
 sys.path.append(os.path.dirname(os.getcwd()))
 
-from .. import extract_old
+from .. import extract
 
 def test_run():
-    extract_old.run(image_data="data/everglades-watch-subjects.csv",  classification_shp="output/old_workflow.shp")
+    extract.run(image_data="data/everglades-watch-subjects.csv",  classification_shp="output/everglades-watch-classifications.shp",savedir="output/images/")
