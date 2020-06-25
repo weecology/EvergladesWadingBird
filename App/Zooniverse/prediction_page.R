9b0652dc7b2578bc9ad950c3d97f8a63cab92f17
bw4sz <benweinstein2010@gmail.com>
2020-06-25 14:59:59 -0700

    view predictions

--- App/Zooniverse/prediction_page.R
+++ App/Zooniverse/prediction_page.R
@@ -9,7 +9,7 @@ predicted_page<-function(df, selected_boxes){
       titlePanel("Predicted Counts"),
       p("Using a deep learning neural network, we predicted the location and number of individual birds in drone imagery aquired over the colonies. This work is in progress, but demonstrates the potential for broad scale monitering"),
       plotOutput("predicted_time_plot",height = 800,width = 1000),
-      h1("Cypress City 3/25/2020"),
+      h1("View Predictions"),
       selectInput("prediction_site","Site",choices = site_list,selected="CypressCity"),
       leafletOutput("sample_prediction_map",height=800,width=1200),
       h2("Compare Zooniverse to Predicted"),
