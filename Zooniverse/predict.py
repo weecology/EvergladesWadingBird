418b7187af5b9335b34bbf647f91996b1e4b0738
bw4sz <benweinstein2010@gmail.com>
2020-06-25 14:26:41 -0700

    don't loop and map at the same time

--- Zooniverse/predict.py
+++ Zooniverse/predict.py
@@ -122,7 +122,7 @@ if __name__ == "__main__":
     #for path in paths:
         #run(model_path=model_path, tile_path=path, savedir="/orange/ewhite/everglades/predictions")
         
-    futures = client.map(run, path, model_path=model_path,savedir="/orange/ewhite/everglades/predictions")
+    futures = client.map(run, paths, model_path=model_path,savedir="/orange/ewhite/everglades/predictions")
     wait(futures)
     print([x.result() for x in futures])
     
