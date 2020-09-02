ce58862bfe94e8636de808700ee2d8520ac0a466
bw4sz <benweinstein2010@gmail.com>
2020-09-02 15:24:07 -0700

    save crops of nest predictions

--- Zooniverse/nest_detection.py
+++ Zooniverse/nest_detection.py
@@ -115,6 +115,8 @@ def find_rgb_path(paths, site, date):
     
     if not len(paths) == 1:
         raise ValueError("A single tile match is needed, found: {}".format(paths))
+    
+    return paths
 
 def crop(rgb_path, geom, extend_box=8):
     src = rasterio.open(rgb_path)
