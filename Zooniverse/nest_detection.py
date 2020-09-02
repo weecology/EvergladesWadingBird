5e2505e70555389b6ba79f778afa27e0d397ea46
bw4sz <benweinstein2010@gmail.com>
2020-09-02 14:19:32 -0700

    save crops of nest predictions

--- Zooniverse/nest_detection.py
+++ Zooniverse/nest_detection.py
@@ -2,8 +2,11 @@
 import glob
 import geopandas
 import rtree
+import rasterio
 import os
 import pandas as pd
+
+from rasterio.windows import from_bounds
 
 def load_files(dirname):
     """Load shapefiles and concat into large frame"""
@@ -106,5 +109,66 @@ def detect_nests(dirname, savedir):
         
     return filename
 
+def find_rgb_path(paths, site, date):
+    paths = [x for x in paths if site in x]
+    paths = [x for x in paths if date in x]
+    
+    if not len(paths) == 1:
+        raise ValueError("A single tile match is needed, found: {}".format(paths))
+
+def crop(rgb_path, geom, extend_box=8):
+    src = rasterio.open(rgb_path)
+    left, bottom, right, top = geom.bounds
+    window = from_bounds(left - extend_box,
+                             bottom - extend_box,
+                             right + extend_box,
+                             top + extend_box,
+                             transform=src.transform)
+    
+    img = src.read(window=window)
+    return img
+    
+def crop_images(df, rgb_pool):
+    """Crop images for a series of data"""
+    crops = {}
+    for index, row in df.iterrows():
+        #find rgb data
+        rgb_path = find_rgb_path(rgb_pool, row["Site"],row["Date"])
+        datename = "{}_{}_{}".format(index,row["Site"],row["Date"])
+        crops[datename] = crop(rgb_path, row["geometry"])
+        crops.append(img)
+    
+    return crops
+
+def extract_nests(filename, rgb_pool, savedir):
+    gdf = geopandas.read_file(filename)
+    grouped = gdf.groupby("target_ind")
+    for name, group in grouped:
+        #atleast three detections
+        if group.shape[0] < 3:
+            continue
+        
+        #Crop with date names as key
+        crops = crop_images(group, rgb_pool=rgb_pool)
+        
+        #save output
+        dirname =  "{}/{}".format(savedir, name) 
+        os.mkdir(dirname)
+        for datename in crops:
+            filename = "{}/{}.png".format(dirname, datename)
+            crop = crops[datename]
+            cv2.imsave(crop, filename)
+
+def find_files():
+    paths = glob.glob("/orange/ewhite/everglades/WadingBirds2020/**/*.tif",recursive=True)
+    sites = ["Joule","CypressCity","Vacation","JetPort","Jerrod","StartMel","OtherColonies","6th Bridge"]
+    paths = [x for x in paths if any(w in x for w in sites)]
+    paths = [x for x in paths if not "projected" in x]
+    
+    return paths
+
 if __name__=="__main__":
-    detect_nests("/orange/ewhite/everglades/predictions/", savedir="../App/Zooniverse/data/")
+    nest_shp = detect_nests("/orange/ewhite/everglades/predictions/", savedir="../App/Zooniverse/data/")
+    #Write nests into folders of clips
+    rgb_pool = find_files()
+    extract_nests(nest_shp, rgb_pool=rgb_pool, savedir="/orange/ewhite/everglades/nest_crops/")
--- Zooniverse/tests/test_nest_prediction.py
+++ Zooniverse/tests/test_nest_prediction.py
@@ -20,6 +20,23 @@ def test_detect_nests():
     filename = nest_detection.detect_nests("data/predictions/",savedir="output/")
     
     assert os.path.exists(filename)
+
+#only run on hipergator with access to the rgb data
+is_travis = 'TRAVIS' in os.environ
+@pytest.mark.skipif(is_travis, reason="Cannot load comet on TRAVIS")
+def test_detect_nests():
+    filename = nest_detection.detect_nests("data/predictions/",savedir="output/")
+    rgb_pool = nest_detection.find_files()
+    nest_detection.extract_nests(filename, rgb_pool=rgb_pool, savedir="output")
+    df = geopandas.read_file(filename)
+    grouped = gdf.groupby("target_ind")
+    for name, group in grouped:
+        if group.shape[0] < 3:
+            continue
+        for index,row in group.iterrows():
+            filename = "output/{}/{}_{}_{}".format(index, row["target_ind"],row["Site"],row["Date"])
+            os.path.exists(filename)
+    
     
     
     
