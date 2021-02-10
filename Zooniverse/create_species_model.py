07a97706af33402498b6d52c6a74e682d2e60f3e
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-02-10 15:11:17 -0800

    species model

---
+++ Zooniverse/create_species_model.py
@@ -0,0 +1,300 @@
+#DeepForest bird detection from extracted Zooniverse predictions
+import comet_ml
+from deepforest import deepforest
+import geopandas as gp
+from shapely.geometry import Point, box
+import pandas as pd
+import rasterio
+import os
+import numpy as np
+import glob
+from datetime import datetime
+
+#Define shapefile utility
+def shapefile_to_annotations(shapefile, rgb_path, savedir="."):
+    """
+    Convert a shapefile of annotations into annotations csv file for DeepForest training and evaluation
+    Args:
+        shapefile: Path to a shapefile on disk. If a label column is present, it will be used, else all labels are assumed to be "Tree"
+        rgb_path: Path to the RGB image on disk
+        savedir: Directory to save csv files
+    Returns:
+        None: a csv file is written
+    """
+    #Read shapefile
+    gdf = gp.read_file(shapefile)
+    
+    #Drop any rounding errors duplicated
+    gdf = gdf.groupby("selected_i").apply(lambda x: x.head(1))
+    
+    #define in image coordinates and buffer to create a box
+    gdf["geometry"] =[Point(x,y) for x,y in zip(gdf.x.astype(float), gdf.y.astype(float))]
+    gdf["geometry"] = [box(int(left), int(bottom), int(right), int(top)) for left, bottom, right, top in gdf.geometry.buffer(25).bounds.values]
+        
+    #extent bounds
+    df = gdf.bounds
+    
+    #Assert size mantained
+    assert df.shape[0] == gdf.shape[0]
+    
+    df = df.rename(columns={"minx":"xmin","miny":"ymin","maxx":"xmax","maxy":"ymax"})
+    
+    #cut off on borders
+    try:
+        with rasterio.open(rgb_path) as src:
+            height, width = src.shape
+    except:
+        print("Image {} failed to open".format(rgb_path))
+        os.remove(rgb_path)
+        return None
+    
+    df.ymax[df.ymax > height] = height
+    df.xmax[df.xmax > width] = width
+    df.ymin[df.ymin < 0] = 0
+    df.xmin[df.xmin < 0] = 0
+    
+    #add filename and bird labels
+    df["image_path"] = os.path.basename(rgb_path)
+    df["label"] = gdf["species"]
+    
+    #remove undesired classes
+    df = df[~(df.label == "Unknown White Small")]
+    
+    #enforce pixel rounding
+    df.xmin = df.xmin.astype(int)
+    df.ymin = df.ymin.astype(int)
+    df.xmax = df.xmax.astype(int)
+    df.ymax = df.ymax.astype(int)
+    
+    #select columns
+    result = df[["image_path","xmin","ymin","xmax","ymax","label"]]
+     
+    result = result.drop_duplicates()
+    
+    return result
+
+def find_rgb_path(shp_path, image_dir):
+    basename = os.path.splitext(os.path.basename(shp_path))[0]
+    rgb_path = "{}/{}.png".format(image_dir,basename)
+    return rgb_path
+    
+def format_shapefiles(shp_dir,image_dir=None):
+    """
+    Format the shapefiles from extract.py into a list of annotations compliant with DeepForest -> [image_name, xmin,ymin,xmax,ymax,label]
+    shp_dir: directory of shapefiles
+    image_dir: directory of images. If not specified, set as shp_dir
+    """
+    if not image_dir:
+        image_dir = shp_dir
+        
+    shapefiles = glob.glob(os.path.join(shp_dir,"*.shp"))
+    
+    #Assert all are unique
+    assert len(shapefiles) == len(np.unique(shapefiles))
+    
+    annotations = [ ]
+    for shapefile in shapefiles:
+        rgb_path = find_rgb_path(shapefile, image_dir)
+        result = shapefile_to_annotations(shapefile, rgb_path)
+        #skip invalid files
+        if result is None:
+            continue
+        annotations.append(result)
+    annotations = pd.concat(annotations)
+    
+    return annotations
+
+def split_test_train(annotations):
+    """Split annotation in train and test by image"""
+    #Currently want to mantain the random split
+    np.random.seed(0)
+    
+    #unique annotations for the bird detector
+    #annotations = annotations.groupby("selected_i").apply(lambda x: x.head(1))
+    
+    #add to train_names until reach target split threshold
+    image_names = annotations.image_path.unique()
+    target = int(annotations.shape[0] * 0.9)
+    counter = 0
+    train_names = []
+    for x in image_names:
+        if target > counter:
+            train_names.append(x)
+            counter+=annotations[annotations.image_path == x].shape[0]
+        else:
+            break
+        
+    train = annotations[annotations.image_path.isin(train_names)]
+    test = annotations[~(annotations.image_path.isin(train_names))]
+    
+    return train, test
+    
+def is_empty(precision_curve, threshold):
+    precision_curve.score = precision_curve.score.astype(float)
+    precision_curve = precision_curve[precision_curve.score > threshold]
+    
+    return precision_curve.empty
+
+def empty_image(precision_curve, threshold):
+    empty_true_positives = 0
+    empty_false_negatives = 0
+    for name, group in precision_curve.groupby('image'): 
+        if is_empty(group, threshold):
+            empty_true_positives +=1
+        else:
+            empty_false_negatives+=1
+    empty_recall = empty_true_positives/float(empty_true_positives + empty_false_negatives)
+    
+    return empty_recall
+
+def plot_recall_curve(precision_curve, invert=False):
+    """Plot recall at fixed interval 0:1"""
+    recalls = {}
+    for i in np.linspace(0,1,11):
+        recalls[i] = empty_image(precision_curve=precision_curve, threshold=i)
+    
+    recalls = pd.DataFrame(list(recalls.items()), columns=["threshold","recall"])
+    
+    if invert:
+        recalls["recall"] = 1 - recalls["recall"].astype(float)
+    
+    ax1 = recalls.plot.scatter("threshold","recall")
+    
+    return ax1
+    
+def predict_empty_frames(model, empty_images, comet_experiment, invert=False):
+    """Optionally read a set of empty frames and predict
+        Args:
+            invert: whether the recall should be relative to empty images (default) or non-empty images (1-value)"""
+    
+    #Create PR curve
+    precision_curve = [ ]
+    for path in empty_images:
+        boxes = model.predict_image(path, return_plot=False)
+        boxes["image"] = path
+        precision_curve.append(boxes)
+    
+    precision_curve = pd.concat(precision_curve)
+    recall_plot = plot_recall_curve(precision_curve, invert=invert)
+    value = empty_image(precision_curve, threshold=0.4)
+    
+    if invert:
+        value = 1 - value
+        metric_name = "BirdRecall_at_0.4"
+        recall_plot.set_title("Atleast One Bird Recall")
+    else:
+        metric_name = "EmptyRecall_at_0.4"
+        recall_plot.set_title("Empty Recall")        
+        
+    comet_experiment.log_metric(metric_name,value)
+    comet_experiment.log_figure(recall_plot)    
+    
+def train_model(train_path, test_path, empty_images_path=None, save_dir=".", comet_experiment=None):
+    """Train a DeepForest model"""
+    model = deepforest.deepforest()
+    model.use_release()
+    comet_experiment.log_parameters(model.config)
+    
+    #Log the number of training and test
+    train = pd.read_csv(train_path)
+    test = pd.read_csv(test_path)
+    comet_experiment.log_parameter("Training_Annotations",train.shape[0])    
+    comet_experiment.log_parameter("Testing_Annotations",test.shape[0])
+    
+    #Set config and train
+    model.config["validation_annotations"] = test_path
+    model.config["save_path"] = save_dir
+    model.config["epochs"] = 9
+    
+    model.train(train_path, comet_experiment=comet_experiment)
+    
+    #Create a positive bird recall curve
+    test_frame_df = pd.read_csv(test_path, names=["image_name","xmin","ymin","xmax","ymax","label"])
+    dirname = os.path.dirname(test_path)
+    test_frame_df["image_path"] = test_frame_df["image_name"].apply(lambda x: os.path.join(dirname,x))
+    empty_images = test_frame_df.image_path.unique()    
+    predict_empty_frames(model, empty_images, comet_experiment, invert=True)
+    
+    #Test on empy frames
+    if empty_images_path:
+        empty_frame_df = pd.read_csv(empty_images_path)
+        empty_images = empty_frame_df.image_path.unique()    
+        predict_empty_frames(model, empty_images, comet_experiment)
+    
+    #evaluaate at lower iou_thresholds
+    mAPs = []
+    threshold = []
+    for x in np.arange(0.1,0.5,.05):
+        mAP = model.evaluate_generator(test_path, 
+                                iou_threshold=x, comet_experiment=comet_experiment)
+        threshold.append(x)
+        mAPs.append(mAP)
+    
+    mAPdf = pd.DataFrame({"mAP":mAPs,"IoU_Threshold":threshold})
+    recall_plot = mAPdf.plot.scatter("mAP","IoU_Threshold")
+    recall_plot.set_xlabel("IoU Threshold")
+    recall_plot.set_ylabel("mAP")
+    comet_experiment.log_figure(recall_plot)
+        
+    return model
+    
+def run(shp_dir, empty_frames_path=None, save_dir="."):
+    """Parse annotations, create a test split and train a model"""
+    annotations = format_shapefiles(shp_dir)    
+    comet_experiment = comet_ml.Experiment(api_key="ypQZhYfs3nSyKzOfz13iuJpj2",
+                                           project_name="everglades-species", workspace="bw4sz")
+    
+    #Split train and test
+    train, test = split_test_train(annotations)
+    
+    #Add some empty images to train and test
+    empty_frames_df = pd.read_csv(empty_frames_path, index_col=0)
+    empty_frames_df.sample(n=10)
+    
+    #add some blank annotations
+    empty_frames_df["xmin"] = pd.Series(dtype="Int64")
+    empty_frames_df["ymin"] = pd.Series(dtype="Int64")
+    empty_frames_df["xmax"] = pd.Series(dtype="Int64")
+    empty_frames_df["ymax"] = pd.Series(dtype="Int64")
+    empty_frames_df["label"] = pd.Series(dtype=str)
+    
+    empty_train, empty_test = split_test_train(empty_frames_df)
+    
+    #limit the number of empty
+    train = pd.concat([train, empty_train])
+    test = pd.concat([test, empty_test])
+    
+    #Enforce rounding to pixels, pandas "Int64" dtype for nullable arrays https://pandas.pydata.org/pandas-docs/stable/user_guide/integer_na.html
+    train.xmin = train.xmin.astype("Int64")
+    train.ymin = train.ymin.astype("Int64")
+    train.xmax = train.xmax.astype("Int64")
+    train.ymax = train.ymax.astype("Int64")
+    
+    test.xmin = test.xmin.astype("Int64")
+    test.ymin = test.ymin.astype("Int64")
+    test.xmax = test.xmax.astype("Int64")
+    test.ymax = test.ymax.astype("Int64")
+                
+    #write paths to headerless files alongside data, add a seperate test empty file
+    train_path = "{}/train.csv".format(shp_dir)
+    test_path = "{}/test.csv".format(shp_dir)
+    empty_test_path = "{}/empty_test.csv".format(shp_dir)
+    
+    train.to_csv(train_path, index=False,header=False)
+    test.to_csv(test_path, index=False,header=False)
+    empty_test.to_csv(empty_test_path, index=False)
+    
+    model = train_model(train_path, test_path, empty_test_path, save_dir, comet_experiment)
+    
+    #Save
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    comet_experiment.log_parameter("timestamp",timestamp)
+    model.prediction_model.save("{}/{}.h5".format(save_dir,timestamp))
+    
+if __name__ == "__main__":
+    run(
+        shp_dir="/orange/ewhite/everglades/Zooniverse/parsed_images/",
+        empty_frames_path="/orange/ewhite/everglades/Zooniverse/parsed_images/empty_frames.csv",
+        save_dir="/orange/ewhite/everglades/Zooniverse/predictions/"
+    )
+    
---
+++ Zooniverse/tests/test_create_species_model.py
@@ -0,0 +1,89 @@
+#test deepforest development
+import os
+import sys
+sys.path.append(os.path.dirname(os.getcwd()))
+
+from .. import create_species_model
+from .. import extract
+from .. import aggregate
+
+import pytest
+import rasterio
+import geopandas as gp
+import pandas as pd
+import glob
+
+#Setup method
+@pytest.fixture()
+def extract_images(tmpdir):
+    aggregate.run("data/everglades-watch-classifications.csv", min_version=300, download=False, generate=False, savedir="output",debug=True)
+    extract.run(image_data="data/everglades-watch-subjects.csv",  classification_shp="output/everglades-watch-classifications.shp",savedir=tmpdir)
+
+@pytest.fixture()
+def annotations(extract_images, tmpdir):
+    annotations = create_species_model.format_shapefiles(shp_dir=tmpdir)    
+    return annotations
+
+def test_shapefile_to_annotations(extract_images, tmpdir):
+    rgb_path = glob.glob("{}/*.png".format(tmpdir))[0]
+    shp = "{}/{}.shp".format(tmpdir, os.path.splitext(os.path.basename(rgb_path))[0])
+    df = create_species_model.shapefile_to_annotations(shapefile=shp, rgb_path=rgb_path)
+    assert all(df.columns == ["image_path","xmin","ymin","xmax","ymax","label"])
+    
+    #assert that the coordinates are in the image system
+    with rasterio.open(rgb_path) as src:
+        height, width = src.shape
+        
+    assert (df.iloc[0].xmin >= 0) & (df.iloc[0].xmax <= width)
+    assert (df.iloc[0].ymin >= 0) & (df.iloc[0].ymax <= height)
+    
+    #Assert total number of records
+    gdf = gp.read_file(shp)
+    assert gdf.shape[0] == df.shape[0]
+    
+    #Assert no duplicates
+    gdf_dropped_duplicates = gdf.drop_duplicates()
+    assert gdf_dropped_duplicates.shape[0] == gdf.shape[0]    
+
+def test_empty_image():
+    image = ["a","a","a","b","b"]
+    scores = ["0.1","0.1","0.1","0.2","0.9"]
+    precision_curve = pd.DataFrame({"image":image,"score":scores})
+    empty_recall = create_species_model.empty_image(precision_curve, threshold=0.15)
+    assert empty_recall == 0.5
+
+def test_plot_recall_curve():
+    image = ["a","a","a","b","b"]
+    scores = ["0.1","0.1","0.1","0.2","0.9"]
+    precision_curve = pd.DataFrame({"image":image,"score":scores})
+    
+    ax1 = create_species_model.plot_recall_curve(precision_curve)
+
+def test_format_shapefiles(extract_images, tmpdir):
+    results = create_species_model.format_shapefiles(shp_dir=tmpdir)
+    assert all(results.columns == ["image_path","xmin","ymin","xmax","ymax","label"])
+    assert results.xmin.dtype == int
+    
+    #Assert no duplicates
+    results_dropped_duplicates = results.drop_duplicates()
+    assert results_dropped_duplicates.shape[0] == results.shape[0]
+    
+def test_split_test_train(extract_images, annotations):
+    
+    train, test = create_species_model.split_test_train(annotations)
+    
+    #Assert no overlapping cases and known deepforest format
+    assert not test.empty
+    assert not train.empty
+    assert all(train.columns == ["image_path","xmin","ymin","xmax","ymax","label"])
+    assert all(test.columns == ["image_path","xmin","ymin","xmax","ymax","label"])
+    assert all(test.label == "Bird")
+    assert all(train.label == "Bird")
+    assert test[test.image_path.isin(train.image_path.unique())].empty
+    
+    #Assert that data is same total sum
+    assert annotations.shape[0] == (test.shape[0] + train.shape[0])
+    
+    #Assert no duplicates
+    train_dropped_duplicates = train.drop_duplicates()
+    assert train_dropped_duplicates.shape[0] == train.shape[0]    
