6d7307419962cdaccc65528600573eb6e7d9061b
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-02-25 11:13:07 -0800

    new folder for the analysis for the bird detector paper

---
+++ bird_detector_paper/ZeroShot.py
@@ -0,0 +1,104 @@
+"""Script to take the trained everglades model and predict the Palmyra data"""
+
+from deepforest import deepforest
+from matplotlib import pyplot as plt
+from shapely.geometry import Point, box
+import geopandas as gpd
+import shapely
+import rasterio as rio
+import numpy as np
+import os
+
+def shapefile_to_annotations(shapefile, rgb, savedir="."):
+    """
+    Convert a shapefile of annotations into annotations csv file for DeepForest training and evaluation
+    Args:
+        shapefile: Path to a shapefile on disk. If a label column is present, it will be used, else all labels are assumed to be "Tree"
+        rgb: Path to the RGB image on disk
+        savedir: Directory to save csv files
+    Returns:
+        results: a pandas dataframe
+    """
+    #Read shapefile
+    gdf = gpd.read_file(shapefile)
+    gdf = gdf[~gdf.geometry.isnull()]
+        
+    #raster bounds
+    with rio.open(rgb) as src:
+        left, bottom, right, top = src.bounds
+        resolution = src.res[0]
+        
+    #define in image coordinates and buffer to create a box
+    gdf["geometry"] = gdf.geometry.boundary.centroid
+    gdf["geometry"] =[Point(x,y) for x,y in zip(gdf.geometry.x.astype(float), gdf.geometry.y.astype(float))]
+    gdf["geometry"] = [box(int(left), int(bottom), int(right), int(top)) for left, bottom, right, top in gdf.geometry.buffer(1.5/resolution).bounds.values]
+        
+    #get coordinates
+    df = gdf.geometry.bounds
+    df = df.rename(columns={"minx":"xmin","miny":"ymin","maxx":"xmax","maxy":"ymax"})    
+    
+    #Transform project coordinates to image coordinates
+    df["tile_xmin"] = (df.xmin - left)/resolution
+    df["tile_xmin"] = df["tile_xmin"].astype(int)
+    
+    df["tile_xmax"] = (df.xmax - left)/resolution
+    df["tile_xmax"] = df["tile_xmax"].astype(int)
+    
+    #UTM is given from the top, but origin of an image is top left
+    
+    df["tile_ymax"] = (top - df.ymin)/resolution
+    df["tile_ymax"] = df["tile_ymax"].astype(int)
+    
+    df["tile_ymin"] = (top - df.ymax)/resolution
+    df["tile_ymin"] = df["tile_ymin"].astype(int)    
+    
+    #Add labels is they exist
+    if "label" in gdf.columns:
+        df["label"] = gdf["label"]
+    else:
+        df["label"] = "Tree"
+    
+    #add filename
+    df["image_path"] = os.path.basename(rgb)
+    
+    #select columns
+    result = df[["image_path","tile_xmin","tile_ymin","tile_xmax","tile_ymax","label"]]
+    result = result.rename(columns={"tile_xmin":"xmin","tile_ymin":"ymin","tile_xmax":"xmax","tile_ymax":"ymax"})
+    
+    #ensure no zero area polygons due to rounding to pixel size
+    result = result[~(result.xmin == result.xmax)]
+    result = result[~(result.ymin == result.ymax)]
+    
+    return result
+
+df = shapefile_to_annotations(shapefile="data/TNC_Dudley_annotation.shp", rgb="/Users/benweinstein/Dropbox/Weecology/bird_detector/Weinstein_TNC_collaboration/Palmyra Tiles (53 m)/20201113_1617-Dudley-A1-im2-206-75x80_transparent_mosaic_group1.tif")
+df.to_csv("Figures/annotations.csv")
+
+src = rio.open("/orange/ewhite/everglades/Palmyra/palymra.tif")
+numpy_image = src.read()
+numpy_image = np.moveaxis(numpy_image,0,2)
+numpy_image = numpy_image[:,:,:3].astype("float32")
+
+model_path = "/orange/ewhite/everglades/Zooniverse/predictions/20210131_015711.h5"
+model = deepforest.deepforest(weights=model_path)
+model.config["save_path"] = "/orange/ewhite/everglades/Palmyra/"
+
+boxes = model.predict_tile(numpy_image=numpy_image, return_plot=False, patch_size=2500)
+bounds = src.bounds
+pixelSizeX, pixelSizeY  = src.res
+
+#subtract origin. Recall that numpy origin is top left! Not bottom left.
+boxes["xmin"] = (boxes["xmin"] *pixelSizeX) + bounds.left
+boxes["xmax"] = (boxes["xmax"] * pixelSizeX) + bounds.left
+boxes["ymin"] = bounds.top - (boxes["ymin"] * pixelSizeY) 
+boxes["ymax"] = bounds.top - (boxes["ymax"] * pixelSizeY)
+
+# combine column to a shapely Box() object, save shapefile
+boxes['geometry'] = boxes.apply(lambda x: shapely.geometry.box(x.xmin,x.ymin,x.xmax,x.ymax), axis=1)
+boxes = gpd.GeoDataFrame(boxes, geometry='geometry')
+
+boxes.crs = src.crs.to_wkt()
+boxes.to_file("figures/predictions.shp")
+
+#Evaluate against model
+model.evaluate_generator("Figures/annotations.csv")
