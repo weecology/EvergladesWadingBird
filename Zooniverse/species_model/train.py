21446282bc2b2d303369e9e15602fe29c211c2fc
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-03-28 15:24:01 -0700

    num samples

--- Zooniverse/species_model/train.py
+++ Zooniverse/species_model/train.py
@@ -148,7 +148,7 @@ def train_model(train_path, test_path, empty_images_path=None, save_dir=".", deb
         image_weight = sum([class_weights[x] for x in labels])/len(labels)
         data_weights.append(1-image_weight)
         
-    sampler = torch.utils.data.sampler.WeightedRandomSampler(weights = data_weights)
+    sampler = torch.utils.data.sampler.WeightedRandomSampler(weights = data_weights, num_samples=len(ds))
     dataloader = torch.data.utils.DataLoader(ds, batch_size = model.config["batch_size"], shuffle=True, sampler = sampler, collate_fn=utilities.collate_fn, num_workers=model.config["workers"])
     
     model.trainer.fit(model, dataloader)
