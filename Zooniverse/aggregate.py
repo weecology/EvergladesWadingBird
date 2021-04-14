9f55e69c2979465ae7081cf9f3972c36e7f9a52d
bw4sz <benweinstein2010@gmail.com>
2021-04-14 14:06:01 -0400

    add client args

--- Zooniverse/aggregate.py
+++ Zooniverse/aggregate.py
@@ -343,7 +343,7 @@ def calculate_IoU(geom, match):
     
     return iou
 
-def run(classifications_file=None, savedir=".", download=False, generate=False,min_version=300, debug=False):
+def run(classifications_file=None, savedir=".", download=False, generate=False,min_version=300, debug=False, client=None):
     
     #Authenticate
     if download:
@@ -375,7 +375,7 @@ def run(classifications_file=None, savedir=".", download=False, generate=False,m
     #save an unprojected copy
     geoms = [Point(x,y) for x,y in zip(df.x, df.y)]
     unprojected_data_gdf = gpd.GeoDataFrame(df, geometry=geoms)
-    unprojected_data_gdf = spatial_join(unprojected_data_gdf, buffer_size=100)    
+    unprojected_data_gdf = spatial_join(unprojected_data_gdf, buffer_size=100, client=client)    
     fname = "{}/{}.shp".format(savedir, "everglades-watch-classifications_unprojected")
     unprojected_data_gdf=unprojected_data_gdf.drop(columns=["bbox"])    
     unprojected_data_gdf.to_file(fname)    
@@ -384,7 +384,7 @@ def run(classifications_file=None, savedir=".", download=False, generate=False,m
     projected_data_gdf = project_point(projected_data)
     
     #Find overlapping annotations and select annotations. Vote on best class for final box
-    projected_data_gdf = spatial_join(projected_data_gdf, buffer_size=1)
+    projected_data_gdf = spatial_join(projected_data_gdf, buffer_size=1, client=client)
     
     #write shapefile
     projected_data_gdf=projected_data_gdf.drop(columns=["bbox"])
@@ -398,7 +398,7 @@ def run(classifications_file=None, savedir=".", download=False, generate=False,m
 if __name__ == "__main__":
     #Download from Zooniverse and parse
     #Optional dask client
-    client = start_cluster(cpus=20)
+    client = start_cluster.start(cpus=20)
     
     fname = run(savedir="../App/Zooniverse/data/", download=True, 
        generate=False, min_version=300, client=client)
