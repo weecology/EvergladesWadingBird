2e41de44fe6a8508d667389d8897af3eca8ace31
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-04-15 15:28:39 -0700

    update generalization

--- bird_detector_paper/training_loop.py
+++ bird_detector_paper/training_loop.py
@@ -70,7 +70,7 @@ def shapefile_to_annotations(shapefile, rgb, savedir="."):
     if "label" in gdf.columns:
         df["label"] = gdf["label"]
     else:
-        df["label"] = "Tree"
+        df["label"] = "Bird"
     
     #add filename
     df["image_path"] = os.path.basename(rgb)
@@ -178,7 +178,7 @@ def training(proportion, epochs=20, patch_size=2000,pretrained=True, iteration=N
     comet_logger.experiment.log_parameter("training_annotations",train_annotations.shape[0])
         
     if pretrained:
-        model = main.deepforest.load_from_checkpoint("/orange/ewhite/everglades/Zooniverse/predictions//20210404_180042/species_model.pl")
+        model = main.deepforest.load_from_checkpoint("/orange/ewhite/everglades/Zooniverse/predictions/20210414_225905/species_model.pl")
         
     else:
         model = main.deepforest()
@@ -218,50 +218,6 @@ def training(proportion, epochs=20, patch_size=2000,pretrained=True, iteration=N
             comet_logger.experiment.log_metric("Box Precision",test_results["box_precision"])
         except Exception as e:
             print(e)
-                
-    ##Evaluate against model
-    #src = rio.open("/orange/ewhite/everglades/Palmyra/palmyra.tif")
-    #numpy_image = src.read()
-    #numpy_image = np.moveaxis(numpy_image,0,2)
-    #numpy_image = numpy_image[:,:,:3].astype("uint8")    
-    #boxes = model.predict_tile(image=numpy_image, return_plot=False, patch_size=patch_size, patch_overlap=0.05)
-    
-    #if boxes is None:
-        #return 0,0
-    
-    #bounds = src.bounds
-    #pixelSizeX, pixelSizeY  = src.res
-    
-    ##subtract origin. Recall that numpy origin is top left! Not bottom left.
-    #boxes["xmin"] = (boxes["xmin"] *pixelSizeX) + bounds.left
-    #boxes["xmax"] = (boxes["xmax"] * pixelSizeX) + bounds.left
-    #boxes["ymin"] = bounds.top - (boxes["ymin"] * pixelSizeY) 
-    #boxes["ymax"] = bounds.top - (boxes["ymax"] * pixelSizeY)
-    
-    ## combine column to a shapely Box() object, save shapefile
-    #boxes['geometry'] = boxes.apply(lambda x: shapely.geometry.box(x.xmin,x.ymin,x.xmax,x.ymax), axis=1)
-    #boxes = gpd.GeoDataFrame(boxes, geometry='geometry')
-    
-    #boxes.crs = src.crs.to_wkt()
-    #boxes.to_file("Figures/predictions_{}.shp".format(proportion))
-    #comet_logger.experiment.log_asset("Figures/predictions_{}.shp".format(proportion))
-    
-    ##define in image coordinates and buffer to create a box
-    #gdf = gpd.read_file("/orange/ewhite/everglades/Palmyra/TNC_Dudley_annotation.shp")
-    #gdf = gdf[~gdf.geometry.isnull()]
-    #gdf["geometry"] = gdf.geometry.boundary.centroid
-    #gdf["geometry"] =[Point(x,y) for x,y in zip(gdf.geometry.x.astype(float), gdf.geometry.y.astype(float))]
-    #gdf["geometry"] = [box(left, bottom, right, top) for left, bottom, right, top in gdf.geometry.buffer(0.15).bounds.values]
-    
-    #results = IoU.compute_IoU(gdf, boxes)
-    #results["match"] = results.IoU > 0.25
-    
-    #results.to_csv("Figures/iou_dataframe_{}.csv".format(proportion))
-    #comet_logger.experiment.log_asset("Figures/iou_dataframe_{}.csv".format(proportion))
-    
-    #true_positive = sum(results["match"] == True)
-    #recall = true_positive / results.shape[0]
-    #precision = true_positive / boxes.shape[0]
     
     recall = test_results["box_recall"]
     precision = test_results["box_precision"]
@@ -278,6 +234,12 @@ def training(proportion, epochs=20, patch_size=2000,pretrained=True, iteration=N
     for img in images:
         comet_logger.experiment.log_image(img)
     
+    #log training images
+    model.predict_file(csv_file = model.config["train"]["csv_file"], root_dir = model.config["train"]["root_dir"], savedir=model_savedir)
+    images = glob.glob("{}/*.png".format(model_savedir))
+    for img in images:
+        comet_logger.experiment.log_image(img)  
+        
     comet_logger.experiment.end()
 
     formatted_results = pd.DataFrame({"proportion":[proportion], "pretrained": [pretrained], "annotations": [train_annotations.shape[0]],"precision": [precision],"recall": [recall], "iteration":[iteration]})
@@ -337,5 +299,5 @@ def run(patch_size=2500, generate=False, client=None, epochs=10, ratio=2):
 
 if __name__ == "__main__":
     #client = start_cluster.start(gpus=5, mem_size="25GB")
-    for x in [1,2,3]:
-        run(client=None, patch_size=1000, epochs=10, ratio=x)
+    for x in [1000,1500,2000]:
+        run(client=None, patch_size=1000, epochs=10, ratio=2)
