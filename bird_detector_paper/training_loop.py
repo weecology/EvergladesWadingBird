13537f7990e8949acabcd250b063ab878efdd68e
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-04-16 17:21:41 -0700

    bring in second training data

--- bird_detector_paper/training_loop.py
+++ bird_detector_paper/training_loop.py
@@ -120,24 +120,24 @@ def prepare_train(patch_size=2000):
         allow_empty=False
     )
     
-    #src = rio.open("/orange/ewhite/everglades/Palmyra/CooperEelPond_53M.tif")
-    #numpy_image = src.read()
-    #numpy_image = np.moveaxis(numpy_image,0,2)
-    #training_image = numpy_image[:,:,:3].astype("uint8")
+    src = rio.open("/orange/ewhite/everglades/Palmyra/CooperEelPond_53M.tif")
+    numpy_image = src.read()
+    numpy_image = np.moveaxis(numpy_image,0,2)
+    training_image = numpy_image[:,:,:3].astype("uint8")
     
-    #df = shapefile_to_annotations(shapefile="/orange/ewhite/everglades/Palmyra/CooperEelPond_53m_annotation.shp", rgb="/orange/ewhite/everglades/Palmyra/CooperEelPond_53M.tif")
+    df = shapefile_to_annotations(shapefile="/orange/ewhite/everglades/Palmyra/CooperEelPond_53m_annotation.shp", rgb="/orange/ewhite/everglades/Palmyra/CooperEelPond_53M.tif")
 
-    #df.to_csv("Figures/training_annotations.csv",index=False)
+    df.to_csv("Figures/training_annotations.csv",index=False)
     
-    #train_annotations_2 = preprocess.split_raster(
-        #numpy_image=training_image,
-        #annotations_file="Figures/training_annotations.csv",
-        #patch_size=patch_size,
-        #patch_overlap=0.05,
-        #base_dir="crops",
-        #image_name="CooperEelPond_53M.tif",
-        #allow_empty=False
-    #)
+    train_annotations_2 = preprocess.split_raster(
+        numpy_image=training_image,
+        annotations_file="Figures/training_annotations.csv",
+        patch_size=patch_size,
+        patch_overlap=0.05,
+        base_dir="crops",
+        image_name="CooperEelPond_53M.tif",
+        allow_empty=False
+    )
     
     train_annotations= pd.concat([train_annotations_1])
     train_annotations.to_csv("crops/full_training_annotations.csv",index=False)
