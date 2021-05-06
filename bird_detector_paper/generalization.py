f82e25a2eed305d4c37afeb2f19659bc58afdf8e
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-05-06 09:28:38 -0700

    write output

--- bird_detector_paper/generalization.py
+++ bird_detector_paper/generalization.py
@@ -521,10 +521,12 @@ if __name__ =="__main__":
     path_dict = prepare()
     view_training(path_dict)
     #leave one out
+    #train_list = ['palmyra',"penguins","terns","pfeifer","hayes"]
     train_list = ["penguins","terns","pfeifer","hayes"]
+    
     results = []
     for x in train_list:
-        train_sets = [y for y in train_list if not y== x]
+        train_sets = [y for y in train_list if not y==x]
         train_sets.append("everglades")
         test_sets = x
         recall, precision = train(path_dict=path_dict, train_sets=train_sets, test_sets=[test_sets])
@@ -532,5 +534,6 @@ if __name__ =="__main__":
         results.append(result)
     
     results = pd.concat(results)
+    results.to_csv("Figures/generalization.csv")
     
     
