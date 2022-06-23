9471ac504473e0782d3845a969359f00fdec1f05
Glenda <gmyenni@users.noreply.github.com>
2022-06-23 10:50:27 -0600

    Add max counts starting 1993 [minor] (#58)
    * Add official colony names and IDs
    * Update species table
    * Update nest checks with correct species and colony names

---
+++ testthat/test-counts.R
@@ -0,0 +1,30 @@
+context("checks count data")
+
+colonies <- read.csv("../SiteandMethods/colonies.csv")
+species <- read.csv("../SiteandMethods/species_list.csv") 
+counts <- read.csv("../Counts/maxcounts.csv")
+counts_cols <- colnames(counts)
+
+test_that("required column names in counts df", {
+  
+  expect_identical(counts_cols, 
+                   c("group_id","year","colony","colony_old","latitude","longitude","wca",      
+                     "species","count","notes"))
+})
+
+test_that("Colony and species valid", {
+  
+  expect_true(all(counts$colony %in% colonies$colony))
+  expect_true(all(counts$species %in% species$species))
+})
+
+test_that("Year valid", {
+
+  expect_false(any(is.na(counts$year)))
+})
+
+test_that("Counts valid", {
+  
+  expect_true(all(is.integer(counts$count)))
+  
+})
