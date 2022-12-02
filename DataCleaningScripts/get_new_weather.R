043ce341cf56ab1d4a155fe66c2a847ca6c085f6
Glenda <gmyenni@users.noreply.github.com>
2022-12-01 17:42:08 -0700

    Add more weather stations [minor] (#77)
    * Add package libraries 
    * Update actions

--- DataCleaningScripts/get_new_weather.R
+++ DataCleaningScripts/get_new_weather.R
@@ -15,54 +15,49 @@
 
 new_met_data <- function() {
   
-  #httr::set_config(httr::timeout(seconds = 120))
+  # Load existing data for comparison
+  weather <- read.csv("Weather/weather.csv") %>%
+    dplyr::mutate(date = lubridate::ymd(date)) %>%
+    dplyr::group_by(station) %>%
+    dplyr::arrange(date)
+  weather_cols <- names(weather)
+  maxdates <- weather %>%
+    dplyr::group_by(station) %>%
+    dplyr::summarize(maxdate = max(date))
   
+  #httr::set_config(httr::timeout(seconds = 120))
+   
   # Pull raw data
   message("Pulling raw weather data")
   
-  oasis <- read.csv(url(
-   'http://www.ncei.noaa.gov/data/global-historical-climatology-network-daily/access/USR0000FOAS.csv'),
-    header = TRUE, sep = ",", stringsAsFactors = FALSE)  %>% 
+  urls <- c('http://www.ncei.noaa.gov/data/global-historical-climatology-network-daily/access/USR0000FOAS.csv',
+            'http://www.ncei.noaa.gov/data/global-historical-climatology-network-daily/access/USR0000FRAC.csv',
+            'http://www.ncei.noaa.gov/data/global-historical-climatology-network-daily/access/US1FLBW0166.csv',
+            'http://www.ncei.noaa.gov/data/global-historical-climatology-network-daily/access/USC00085184.csv',
+            'http://www.ncei.noaa.gov/data/global-historical-climatology-network-daily/access/USC00080737.csv',
+            'http://www.ncei.noaa.gov/data/global-historical-climatology-network-daily/access/US1FLBW0099.csv',
+            'http://www.ncei.noaa.gov/data/global-historical-climatology-network-daily/access/US1FLBW0131.csv',
+            'http://www.ncei.noaa.gov/data/global-historical-climatology-network-daily/access/US1FLBW0103.csv',
+            'http://www.ncei.noaa.gov/data/global-historical-climatology-network-daily/access/US1FLBW0160.csv',
+            'http://www.ncei.noaa.gov/data/global-historical-climatology-network-daily/access/US1FLBW0139.csv')
+  all <- lapply(urls, read.csv, header = TRUE, sep = ",", stringsAsFactors = FALSE)
+  all <- do.call(dplyr::bind_rows, all) %>% 
     dplyr::rename_all(. %>% tolower) %>%
-    dplyr::select(-c(latitude, longitude, name, elevation)) %>%
     dplyr::mutate(date = lubridate::ymd(date),
                   tmax = tmax/10,
                   tmin = tmin/10, 
-                  tavg = tavg/10)
+                  tavg = tavg/10, 
+                  tobs = tobs/10)
   
-  raccoon <- read.csv(url(
-    'http://www.ncei.noaa.gov/data/global-historical-climatology-network-daily/access/USR0000FRAC.csv'),
-    header = TRUE, sep = ",", stringsAsFactors = FALSE)  %>% 
-    dplyr::rename_all(. %>% tolower) %>%
-    dplyr::select(-c(latitude, longitude, name, elevation)) %>%
-    dplyr::mutate(date = lubridate::ymd(date),
-                  tmax = tmax/10,
-                  tmin = tmin/10, 
-                  tavg = tavg/10)
-  
-  # Load existing data for comparison
-  weather <- read.csv("Weather/weather.csv") %>%
-    dplyr::mutate(date = lubridate::ymd(date))
-  weather_cols <- names(weather)
-
-  oasis_latest <- max(weather$date[weather$station=="USR0000FOAS"])
-  raccoon_latest <- max(weather$date[weather$station=="USR0000FRAC"])
-  
-  oasis[,setdiff(weather_cols, names(oasis))] <- NA
-  raccoon[,setdiff(weather_cols, names(raccoon))] <- NA
-  
-  # Keep only new data
-  new_oasis <- oasis %>%
-    dplyr::filter(date > oasis_latest) %>%
-    dplyr::select(weather_cols)
-  
-  new_raccoon <- raccoon %>%
-    dplyr::filter(date > raccoon_latest) %>%
-    dplyr::select(weather_cols)
+  all[,setdiff(weather_cols, names(all))] <- NA
     
   # Set up dataframe to append
     
-  new_data <- dplyr::bind_rows(new_oasis, new_raccoon) %>%
+  new_data <- all %>%
+    dplyr::group_by(station) %>%
+    dplyr::left_join(maxdates, by = "station") %>%
+    dplyr::filter(is.na(maxdate)|date > maxdate) %>%
+    dplyr::select(dplyr::all_of(weather_cols)) %>%
     dplyr::arrange(date)
   
   return(new_data)
