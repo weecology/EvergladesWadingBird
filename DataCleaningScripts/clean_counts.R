bb303a0c3a623420ac858c454dff74b2fbe5a3c9
Glenda <gmyenni@users.noreply.github.com>
2023-06-17 00:10:43 -0600

    Indicators update (#95) [minor]
    * Add indicators script [minor]
    
    * Add colony level counts 2022 [minor]
    
    * Add new enp colonies [minor]

--- DataCleaningScripts/clean_counts.R
+++ DataCleaningScripts/clean_counts.R
@@ -3,6 +3,92 @@
 
 `%>%` <- magrittr::`%>%`
 
+#' Reshapes and cleans max count data (2022->)
+#'
+#'
+#'
+#' Reads count data from original excel files, reshapes from wide (dates as cols) to long format,
+#' corrects data format errors, appends to long timeseries file
+clean_count_data <- function(data_path, year) {
+  
+  colonies <- read.csv("SiteandMethods/colonies.csv")
+  species <- read.csv("SiteandMethods/species_list.csv")
+  
+  tab_names <- readxl::excel_sheets(path = data_path)
+  
+  data_raw <- readxl::read_excel(path = data_path, sheet = "counts", 
+                                 col_names = TRUE, col_types = "text") %>%
+    dplyr::rename_with(tolower) %>%
+    dplyr::rename(smda="unid. small dark.") %>%
+    dplyr::rename(smwh="unid. small white") %>%
+    dplyr::mutate(colony_old = colony,
+                  colony = tolower(colony),
+                  colony = gsub(" ", "_", colony),
+                  colony = gsub("/", "_", colony),
+                  colony = replace(colony, colony=="63_no_name", "63"),
+                  colony = replace(colony, colony=="71_canal_junction", "canal_junction"),
+                  colony = replace(colony, colony=="78_canal_north", "canal_north"),
+                  colony = replace(colony, colony=="3b_ramp_80", "3b_boat_ramp"),
+                  colony = replace(colony, colony=="89_venus", "venus"),
+                  colony = replace(colony, colony=="austere", "auster"),
+                  colony = replace(colony, colony=="cooklox11", "lox111"),
+                  colony = replace(colony, colony=="cooknc4", "lox_nc4"),
+                  colony = replace(colony, colony=="enlil_epona", "enlil"),
+                  colony = replace(colony, colony=="jetport_new_64", "jetport_new"),
+                  colony = replace(colony, colony=="loxwest", "lox_west"),
+                  colony = replace(colony, colony=="loxramp_011", "lox_ramp"),
+                  colony = replace(colony, colony=="tyr_lox73", "lox73"),
+                  colony = replace(colony, colony=="vulture_007", "vulture"),
+                  colony = replace(colony, colony=="1219_draco", "draco"),
+                  colony = replace(colony, colony=="990_frodo", "frodo"),
+                  colony = replace(colony, colony=="38", "38_185"),
+                  colony = replace(colony, colony=="51", "juno")) %>%
+    dplyr::left_join(colonies[,1:2], by = dplyr::join_by(colony))
+  
+  new_colonies <- data_raw[-which(data_raw$colony %in% colonies$colony),] %>%
+    dplyr::mutate(group_id = max(colonies$group_id)+1:dplyr::n(),
+                  region = wca,
+                  display_name = colony) %>% 
+    dplyr::select(group_id,colony,region,subregion,latitude,longitude,aka,display_name)
+#  write.table(new_colonies, "SiteandMethods/colonies.csv", 
+#              row.names = FALSE, col.names = FALSE, na = "", sep = ",", quote = c(7,8), append=TRUE)
+    
+  new_data <- data_raw %>%
+    dplyr::select(-c(wca,group_id,total)) %>%
+    tidyr::pivot_longer(cols = !c(colony,colony_old,latitude,longitude), 
+                          names_to = "species",
+                          values_to = "count") %>%
+    dplyr::mutate(year = year,
+                    notes = "",
+                    notes = replace(notes, count=="***", "presence"),
+                    count = replace(count, count=="***", 1)) %>%
+      
+    dplyr::filter(!is.na(count)) %>%
+    dplyr::left_join(colonies[,1:2], by = dplyr::join_by(colony)) %>%
+    
+    dplyr::mutate(year = as.numeric(year),
+                  latitude = as.numeric(latitude),
+                  longitude = as.numeric(longitude),
+                  count = as.numeric(count)) %>%
+    dplyr::select("group_id","year","colony","colony_old","latitude","longitude","species","count","notes")
+  
+  if(!all(new_data$colony %in% colonies$colony)| 
+     !all(new_data$species %in% species$species)) {
+    print(unique(new_data$colony[which(!(new_data$colony %in% colonies$colony))]))
+    print(unique(new_data$species[which(!(new_data$species %in% species$species))]))
+  }
+  
+  under_40 <- readxl::read_excel(path = data_path, sheet = "under40", 
+                                               col_names = TRUE, col_types = "text") %>%
+    dplyr::rename_with(tolower) %>%
+    dplyr::rename(smda="unid. small dark.") %>%
+    dplyr::rename(smwh="unid. small white") %>%
+    dplyr::mutate(group_id=NA, year=year, colony_old=colony,dcco=NA,grhe=NA,smhe=NA,lawh=NA,lada=NA) %>%
+    dplyr::select("group_id","year","colony","colony_old","latitude","longitude","wca","greg","whib","wost","gbhe","rosp","sneg","anhi","trhe","bcnh","lbhe","ycnh","glib","caeg","dcco","grhe","smhe","lawh","lada","smda","smwh","notes","total")
+    
+  return(list(new_data=new_data, new_colonies=new_colonies, under_40=under_40))
+}
+
 #' Functions customized to old data (-2021)
 #'
 
@@ -13,7 +99,7 @@
 #' Reads count data from original excel files, reshapes from wide (dates as cols) to long format,
 #' corrects data format errors, appends to long timeseries file
 
-clean_count_data <- function(data_path, year) {
+clean_count_data_old <- function(data_path, year) {
   
   colonies <- read.csv("SiteandMethods/colonies.csv")
   species <- read.csv("SiteandMethods/species_list.csv")
