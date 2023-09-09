5cc9f6b51a65878f9dbfb301d094cfdfa9aee118
Glenda <gmyenni@users.noreply.github.com>
2023-09-09 17:29:37 -0600

    Start water updates [minor]

--- update-data.R
+++ update-data.R
@@ -1,3 +1,3 @@
 message("Updating weather station data")
-source("DataCleaningScripts/get_new_weather.R"); append_weather()
-# source("DataCleaningScripts/get_water_data.R"); update_water()
+# source("DataCleaningScripts/get_new_weather.R"); append_weather()
+source("DataCleaningScripts/get_water_data.R"); update_water()
