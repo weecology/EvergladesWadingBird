51be02a0cda97aeb3bf39991c6b8d9313ae1a4bd
Glenda <gmyenni@users.noreply.github.com>
2024-01-11 12:17:05 -0700

    Add depth data [minor] (#102)
    * Update water depths [minor]

---
+++ DataCleaningScripts/download_eden.R
@@ -0,0 +1,136 @@
+# Functions to find and download EDEN water depth data
+#' @name get_metadata
+#'
+#' @title Get EDEN metadata
+#'
+#' @export
+#'
+
+get_metadata <- function() {
+  url <- "https://sflthredds.er.usgs.gov/thredds/catalog/eden/depths/catalog.html"
+  metadata <- url %>%
+    rvest::read_html() %>%
+    rvest::html_table()
+  metadata <- metadata[[1]] %>%
+    dplyr::filter(Dataset != "depths") %>% #Drop directory name from first row
+    dplyr::rename(dataset = Dataset, size = Size, last_modified = `Last Modified`) %>%
+    dplyr::mutate(last_modified = as.POSIXct(last_modified,
+                             format = "%Y-%m-%dT%H:%M:%S"),
+                  year = as.integer(substr(dataset, start = 1, stop = 4)))
+}
+
+#' @name get_data_urls
+#'
+#' @title Get EDEN depths data URLs for download
+#'
+#' @param file_names file names to download from metadata
+#'
+#' @return list of file urls
+#'
+#' @export
+#'
+
+get_data_urls <- function(file_names) {
+  base_url <- "https://sflthredds.er.usgs.gov/thredds/fileServer/eden/depths"
+  urls <- file.path(base_url, file_names)
+  return(list(file_names = file_names, urls = urls))
+}
+
+#' @name get_last_download
+#'
+#' @title Get list of EDEN depths data already downloaded
+#'
+#' @param eden_path path where the EDEN data should be stored
+#' @param metadata EDEN file metadata
+#' @param force_update if TRUE update all data files even if checks indicate
+#'   that remote files are unchanged since the current local copies were
+#'   created
+#'
+#' @return table of files already downloaded
+#'
+#' @export
+#'
+get_last_download <- function(eden_path = file.path("Water"),
+                              metadata, force_update = FALSE) {
+  if ("last_download.csv" %in% list.files(eden_path) & !force_update) {
+    last_download <- read.csv(file.path(eden_path, "last_download.csv"))
+  } else {
+    last_download <- data.frame(dataset = metadata$dataset, size = "0 Mbytes",
+                       last_modified = as.POSIXct("1900-01-01 00:00:01",
+                                         format = "%Y-%m-%d %H:%M:%S"))
+  }
+  return(last_download)
+}
+
+#' @name get_files_to_update
+#'
+#' @title Determine list of new EDEN files to download
+#'
+#' @param eden_path path where the EDEN data should be stored
+#' @param metadata EDEN file metadata
+#' @param force_update if TRUE update all data files even if checks indicate
+#'   that remote files are unchanged since the current local copies were
+#'   created
+#'
+#' @export
+#'
+get_files_to_update <- function(eden_path = file.path("Water"),
+                                metadata, force_update = FALSE){
+  # Find files that have been updated since last download
+  last_download <- get_last_download(eden_path, metadata, force_update = force_update)
+  new <- metadata %>%
+    dplyr::left_join(last_download, by = "dataset", suffix = c(".curr", ".last")) %>%
+    dplyr::filter(last_modified.curr > last_modified.last | size.curr != size.last | is.na(last_modified.last))
+  metadata %>%
+    dplyr::filter(year %in% c(new$year-2, new$year-1, new$year, new$year+1, new$year+2))
+  }
+
+#' @name update_last_download
+#'
+#' @title Write new metata file for files already downloaded
+#'
+#' @param eden_path path where the EDEN data should be stored
+#' @param metadata EDEN file metadata
+#'
+#' @export
+#'
+update_last_download <- function(eden_path = file.path("Water"),
+                                 metadata){
+  current_files <- list.files(eden_path, pattern = "*_depth.nc")
+  write.csv(metadata, file.path(eden_path, 'last_download.csv'))
+}
+
+#' @name download_eden_depths
+#'
+#' @title Download the EDEN depths data
+#'
+#' @param eden_path path where the EDEN data should be stored
+#' @param force_update if TRUE update all data files even if checks indicate
+#'   that remote files are unchanged since the current local copies were
+#'   created
+#'
+#' @return char vector of downloaded/updated files
+#'
+#' @export
+#'
+download_eden_depths <- function(eden_path = file.path("Water"),
+                                 force_update = FALSE) {
+
+  if (!dir.exists(eden_path)) {
+    dir.create(eden_path, recursive = TRUE)
+  }
+
+  metadata <- get_metadata()
+  to_update <- get_files_to_update(eden_path, metadata,
+                                   force_update = force_update)
+  data_urls <- get_data_urls(to_update$dataset)
+  options(timeout = 226)
+
+  downloaded <- mapply(download.file,
+    data_urls$urls,
+    file.path(eden_path, data_urls$file_names))
+
+  update_last_download(eden_path, metadata)
+
+  return(file.path(eden_path, data_urls$file_names))
+}
--- DataCleaningScripts/get_water_data.R
+++ DataCleaningScripts/get_water_data.R
@@ -2,34 +2,46 @@
 #'
 
 `%>%` <- magrittr::`%>%`
+source("DataCleaningScripts/download_eden.R")
+source("DataCleaningScripts/eden_covariates.R")
 
 # Downloads new EDEN depth data, calculates covariates, appends to covariate file
 
 get_eden_data <- function() {
 
-wader::download_eden_depths()
+download_eden_depths()
 
-new_data <- wader::get_eden_covariates()
-new_data2 <- wader::get_eden_covariates(level="all")
-new_data3 <- wader::get_eden_covariates(level="wcas")
+covariate_data <- read.table("Water/eden_covariates.csv", header = TRUE, sep = ",")
+new_data <- get_eden_covariates()
+new_data2 <- get_eden_covariates(level="all")
+new_data3 <- get_eden_covariates(level="wcas")
 all_data <- dplyr::bind_rows(new_data,new_data2,new_data3) %>%
   dplyr::select(year, region=Name, variable, value) %>%
   as.data.frame() %>%
   dplyr::select(-geometry) %>%
   tidyr::pivot_wider(names_from="variable", values_from="value") %>%
   dplyr::arrange("year", "region")
+new_covariates <- all_data %>%
+  merge(dplyr::filter(covariate_data, !date %in% all_data$year)) %>%
+  dplyr::arrange("year", "region")
 
-depth_data <- wader::get_eden_depths() %>%
-              dplyr::bind_rows(wader::get_eden_depths(level="all")) %>%
-              dplyr::bind_rows(wader::get_eden_depths(level="wcas")) %>%
+depth_data <- read.table("Water/eden_depth.csv", header = TRUE, sep = ",") %>%
+              dplyr::mutate(date=as.Date(date))
+new_depths <- get_eden_depths() %>%
+              dplyr::bind_rows(get_eden_depths(level="all")) %>%
+              dplyr::bind_rows(get_eden_depths(level="wcas")) %>%
               dplyr::mutate(date=as.Date(date))
 
-file.remove(dir(path=file.path(get_default_data_path(), 'EvergladesWadingBird/Water'),  pattern="_.*_depth.nc"))
+depth_data <- dplyr::filter(depth_data, !date %in% new_depths$date) %>%
+              rbind(new_depths) %>%
+              dplyr::arrange("date", "region")
 
-return(list(new_covariates=new_covariates, new_depths=new_depths))
+file.remove(dir(path=file.path('Water'),  pattern="_.*_depth.nc"))
+
+return(list(new_covariates=new_covariates, depth_data=depth_data))
 }
 
-#' Appends new water data
+#' Writes new water data
 #'
 #'
 
@@ -37,10 +49,10 @@ update_water <- function() {
 
   data <- get_eden_data()
 
-  write.table(data$new_covariates, "../Water/eden_covariates.csv", row.names = FALSE, col.names = TRUE,
+  write.table(data$new_covariates, "Water/eden_covariates.csv", row.names = FALSE, col.names = TRUE,
             na="", sep = ",", quote = FALSE)
 
-  write.table(data$new_depths, file = "../Water/eden_depth.csv",
+  write.table(data$depth_data, file = "Water/eden_depth.csv",
               row.names = FALSE, col.names = TRUE, na = "", sep = ",", quote = FALSE)
 }
 
