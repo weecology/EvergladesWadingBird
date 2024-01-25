b9d7258e6312b1fac343dc3f231b5c8c6a0eb005
Glenda Yenni <glenda@weecology.org>
2024-01-25 15:20:00 -0700

    Rename subregions [patch]

--- testthat/test-water.R
+++ testthat/test-water.R
@@ -5,6 +5,7 @@ depth <- read.csv(file = "../Water/eden_depth.csv",header=T) %>%
 depth_names <- colnames(depth)
 covariates <- read.csv(file = "../Water/eden_covariates.csv",header=T)
 covariates_names <- colnames(covariates)
+colonies <- read.csv("../SiteandMethods/colonies.csv")
 
 test_that("required column names in new water df", {
 
@@ -13,6 +14,12 @@ test_that("required column names in new water df", {
   expect_identical(covariates_names,
                    c("year","region","init_depth","breed_season_depth", "recession",
                      "pre_recession","post_recession","dry_days","reversals"))
+})
+
+test_that("Subregions valid", {
+  
+  expect_true(all(depth$region %in% c(colonies$subregion,"all","wcas","enp")))
+  expect_true(all(covariates$region %in% c(colonies$subregion,"all","wcas","enp")))
 })
 
 test_that("Dates valid", {
