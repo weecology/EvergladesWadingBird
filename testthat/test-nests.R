01d65802058d7a4e781b20e16b31946cf225955b
Glenda <gmyenni@users.noreply.github.com>
2024-02-13 11:02:27 -0700

    Add metadata files [minor] (#116)
    * Update nest success data and metadata [minor]
    * Update maxcount metadata [minor]

--- testthat/test-nests.R
+++ testthat/test-nests.R
@@ -5,17 +5,25 @@ species <- read.csv("../SiteandMethods/species_list.csv")
 nests <- read.csv("../Nesting/nest_checks.csv") %>%
   dplyr::mutate(date = lubridate::ymd(date))
 nest_cols <- colnames(nests)
+success <- read.csv("../Nesting/nest_success.csv")
+success_cols <- colnames(success)
 
 test_that("required column names in nests df", {
   
   expect_identical(nest_cols, 
                    c("year","colony","nest","species","date","eggs","chicks","stage", "notes"))
+  expect_identical(success_cols, 
+                   c("year","colony","nest_number","species","n_days_incubation","incubation_success",
+                     "n_days_nestling","nestling_success","clutch","brood","fledged","clutch_type",
+                     "young_lost","real_success","real_failure","start_date","end_date","notes"))    
 })
 
 test_that("Colony and species valid", {
   
   expect_true(all(nests$colony %in% colonies$colony))
   expect_true(all(nests$species %in% species$species))
+  expect_true(all(success$colony %in% success$colony))
+  expect_true(all(success$species %in% success$species))
 })
 
 test_that("Dates valid", {
