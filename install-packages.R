255c5718fd787ac1201f838acc0b733548e6076c
Glenda Yenni <glenda@weecology.org>
2024-09-24 15:01:10 -0600

    Add r2u and create release actions [no version bump]

--- install-packages.R
+++ install-packages.R
@@ -1,5 +1,15 @@
-# Install pacman if it isn't already installed
-if ("pacman" %in% rownames(installed.packages()) == FALSE) install.packages("pacman")
-
 # Install packages required for analysis
-pacman::p_load(cubelyr, dplyr, git2r, httr, lubridate, rvest, semver, sf, stars, terra, testthat, tidyr, yaml)
+
+install.packages(c("cubelyr", 
+                   "dplyr", 
+                   "git2r", 
+                   "httr", 
+                   "lubridate", 
+                   "rvest", 
+                   "semver", 
+                   "sf", 
+                   "stars", 
+                   "terra", 
+                   "testthat", 
+                   "tidyr", 
+                   "yaml"))
