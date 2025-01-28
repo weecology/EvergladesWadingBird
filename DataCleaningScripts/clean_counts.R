1982ddfcbd3434bc317f6e0ec5b708b8f60c7458
Glenda Yenni <glenda@weecology.org>
2025-01-27 18:12:06 -0700

    Add 2024 max counts [minor]

--- DataCleaningScripts/clean_counts.R
+++ DataCleaningScripts/clean_counts.R
@@ -5,10 +5,9 @@
 `%>%` <- magrittr::`%>%`
 
 ############################# Get raw data ####################################################
-year <- 2019
-data_path <- "~/Dropbox (UFL)/Everglades/Reports/2019 Reports/SFWMD report table 2019.xlsx"
-data_path <- "~/Desktop/maxcount_2019.xlsx"
-SFWMD_report_table_2019 <- readxl::read_excel(data_path, sheet = "Appendix", skip = 1)
+year <- 2024
+data_path <- "~/Desktop/Appendix 2024.xlsx"
+SFWMD_report_table_2019 <- readxl::read_excel(data_path, sheet = "Appendix")
 
 ############################ Build data tables  #######################################
 
@@ -21,51 +20,19 @@ species <- read.csv("SiteandMethods/species_list.csv")
 counts <- read.csv("Counts/maxcounts.csv")
 under40 <- read.csv("Counts/maxcounts_under40.csv")
   
-  tab_names <- readxl::excel_sheets(path = data_path)
-  
   data_raw <- readxl::read_excel(path = data_path, sheet = "Appendix", 
-                                 col_names = TRUE, col_types = "text", skip=1) %>%
+                                 col_names = TRUE, col_types = "text") %>%
     dplyr::rename_with(tolower) %>%
-    dplyr::rename(smda="unid. small dark.") %>%
-    dplyr::rename(smwh="unid. small wht.") %>%
-    dplyr::rename(lawh="unid. large wht.") %>%
     dplyr::mutate(colony_old = colony,
                   colony = tolower(colony),
                   colony = gsub(" ", "_", colony),
                   colony = gsub("/", "_", colony),
-                  colony = replace(colony, colony %in% c("63_no_name","63_006"), "63"),
-                  colony = replace(colony, colony %in% c("71_canal_junction","71"), "canal_junction"),
-                  colony = replace(colony, colony=="78_canal_north", "canal_north"),
                   colony = replace(colony, colony %in% c("3b_ramp_80","3b_ramp"), "3b_boat_ramp"),
-                  colony = replace(colony, colony=="89_venus", "venus"),
-                  colony = replace(colony, colony=="austere", "auster"),
-                  colony = replace(colony, colony %in% c("cooklox11", "cooknc3"), "lox111"),
-                  colony = replace(colony, colony=="cooknc4", "lox_nc4"),
-                  colony = replace(colony, colony=="enlil_epona", "enlil"),
-                  colony = replace(colony, colony=="jetport_new_64", "jetport_new"),
-                  colony = replace(colony, colony=="loxwest", "lox_west"),
-                  colony = replace(colony, colony=="cooknc1(77_78)", "lox_nc1"),
-                  colony = replace(colony, colony=="cooknc2(76)", "vesta"),
-                  colony = replace(colony, colony=="loxramp_011", "lox_ramp"),
-                  colony = replace(colony, colony %in% c("tyr_lox73","tyr", "lox73_tyr"), "lox73"),
-                  colony = replace(colony, colony=="vulture_007", "vulture"),
-                  colony = replace(colony, colony=="1219_draco", "draco"),
-                  colony = replace(colony, colony=="990_frodo", "frodo"),
-                  colony = replace(colony, colony=="38", "38_185"),
-                  colony = replace(colony, colony %in% c("51", "51_juno"), "juno"),
-                  colony = replace(colony, colony=="lox11", "outer_lox111_south"),
-                  colony = replace(colony, colony=="little_d_little_a", "little_a"),
-                  colony = replace(colony, colony=="112", "3665"),
-                  colony = replace(colony, colony %in% c("1362", "739", "487"), "col487"),
-                  colony = replace(colony, colony %in% c("1470","576", "98"), "1888"),
-                  colony = replace(colony, colony %in% c("1379", "766_57_nc_2018"), "1824"),
-                  colony = replace(colony, colony=="14", "1351"),
-                  colony = replace(colony, colony=="75", "3700"),
-                  colony = replace(colony, colony=="610_67_nc_2018", "67"),
-                  colony = replace(colony, colony=="644", "1573"),
-                  colony = replace(colony, colony=="2019_greg_colony_1", "colony13"),
                   colony = replace(colony, colony %in% c("rodgers_river_bay_large_island","rodgers_river_bay_small_island"), "rodgers_river_bay"),
-                  colony = replace(colony, colony=="grossman_ridge_willowhead", "grossman_willowhead")) %>%
+                  colony = replace(colony, colony=="grossman_ridge_willowhead", "grossman_willowhead"),
+                  colony = replace(colony, colony=="austere", "auster"),
+                  colony = replace(colony, colony=="lox_99", "lox99"),
+                  colony = replace(colony, colony=="lox_11", "lox11")) %>%
     dplyr::left_join(colonies[,1:2], by = dplyr::join_by(colony))
   
   new_colonies <- data_raw[-which(data_raw$colony %in% colonies$colony),]
@@ -74,8 +41,7 @@ under40 <- read.csv("Counts/maxcounts_under40.csv")
   ######################## Add new colony info to colonies table  ################################
   
   ### New colony
-  # colonies[dim(colonies)[1]+1,]=c(max(colonies$group_id,na.rm=T) + 1,
-  # "colony","region","subregion",latitude,longitude,"aka","Display Name")
+  # colonies[dim(colonies)[1]+1,]=c(1837,"36","3","3as", 25.974284,	-80.724161,"","36")
   
   ### Update colony
   # colonies[colonies$colony=="colony13",]$aka="Colony 13, 2019 GREG colony 1"
@@ -91,8 +57,11 @@ under40 <- read.csv("Counts/maxcounts_under40.csv")
 ############################### Shape max count new data ###################################
   
   new_data <- data_raw %>%
-    dplyr::select(-c(wca,total)) %>%
+    dplyr::select(-c(wca,total,"coordinate notes")) %>%
     dplyr::filter(colony %in% colonies$colony) %>%
+    # check for transect waypoints misnamed as colonies
+    dplyr::filter(!(colony %in% c("1309", "1380", "1980", "1824", "1351", "1888", "968", 
+                                  "2013", "2049", "1844", "1882"))) %>%
     tidyr::pivot_longer(cols = !c(group_id,colony,colony_old,latitude,longitude), 
                           names_to = "species",
                           values_to = "count") %>%
@@ -102,7 +71,6 @@ under40 <- read.csv("Counts/maxcounts_under40.csv")
                     count = replace(count, count=="***", 1)) %>%
     dplyr::filter(!is.na(count)) %>%
     dplyr::left_join(colonies[,1:2], by = dplyr::join_by(group_id,colony)) %>%
-    
     dplyr::mutate(year = as.numeric(year),
                   latitude = as.numeric(latitude),
                   longitude = as.numeric(longitude),
@@ -116,18 +84,19 @@ under40 <- read.csv("Counts/maxcounts_under40.csv")
   }
 
 ############################## Shape under 40 new data  ##############################################
-### Only colonies < 40 should be left in new_colonies
+### Only colonies < 40 should be left in new_colonies and no transect numbers should be left
+  # in new_data
   
-  under_40_new <- new_colonies %>%
-    dplyr::filter(!is.na(latitude)) %>%
-    dplyr::select(-"smda") %>%
+  under_40_new <- data_raw %>%
+    dplyr::filter(!is.na(latitude),
+                  !(colony %in% new_data$colony)) %>%
     dplyr::rowwise() %>%
     dplyr::mutate(notes = dplyr::case_when(
                           any(dplyr::c_across(greg:anhi)=="***", na.rm = T) ~ "1s indicate presence",
                           TRUE ~ ""))
   under_40_new[under_40_new=="***"] <- "1"
   under_40_new <- under_40_new %>%
-    dplyr::mutate(year=as.numeric(year), dcco = NA, smhe=NA, lada=NA) %>%
+    dplyr::mutate(year=as.numeric(year), dcco = NA, smhe=NA, lada=NA, lawh=NA) %>%
     dplyr::select("group_id","year","colony","colony_old","latitude","longitude","wca","greg","whib","wost","gbhe","rosp","sneg","anhi","trhe","bcnh","lbhe","ycnh","glib","caeg","dcco","grhe","smhe","lawh","lada","smwh","total","notes") %>%
     dplyr::mutate_at(c("group_id","year","latitude","longitude","greg","whib","wost","gbhe","rosp","sneg","anhi","trhe","bcnh","lbhe","ycnh","glib","caeg","dcco","grhe","smhe","lawh","lada","smwh","total"),as.numeric)
     
